c07666ca4284ab4025de4d94968afc5be84a6803
Janith Sandeepa <janithsandeepa@example.com>
2026-03-29 09:24:26 +0530

    Update website favicon and meta images to use JapLan Tours logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,7 +32,7 @@ const Navbar = () => {
       <div className="container mx-auto flex items-center justify-between py-4 px-6">
         <a href="/" className="flex items-center gap-3">
           <img 
-            src="/logo.png" 
+            src="/japlan-logo.svg" 
             alt="JapLan Tours" 
             className="h-10 w-10 object-contain"
           />
